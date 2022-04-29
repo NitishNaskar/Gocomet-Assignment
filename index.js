840e618ae87fe732eddb4a869b58614bb1820087
Nitish Naskar <naskarnitish18@gmail.com>
2022-04-29 20:16:53 +0530

    setup node.js router and controller

---
+++ index.js
@@ -0,0 +1,19 @@
+const express=require("express");
+const app=express();
+const port=8000;
+
+// set view engine
+app.set("view engine","ejs");
+app.set("views","./views");
+
+// Routes for index
+app.use('/',require("./routes/index"));
+
+app.listen(port,function(err){
+    if(err){
+        console.log("Some error in this surver >> ",err);
+        return;
+    }
+
+    console.log("OK ! your server is running ......");
+});
